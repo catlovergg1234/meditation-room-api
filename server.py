d4cb4ee7363f4e6b3fe1fe2c92e42c397518ae53
catlovergg1234 <yadedorbek@gmail.com>
2025-07-07 22:37:00 +0300

    fix: added host=0.0.0.0 and port to stop server running on localhost

--- server.py
+++ server.py
@@ -54,4 +54,4 @@ def log_in():
         return jsonify({"status": "error", "reason": "password incorrect"})
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
