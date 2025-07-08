033a86ecbc1343b854cf326d73ac91eeff3da61c
catlovergg1234 <yadedorbek@gmail.com>
2025-07-08 19:40:09 +0300

    Yeah I thought before_request didn't work but os.getenv returns None sooo I fixed it

--- server.py
+++ server.py
@@ -19,39 +19,36 @@ def hash(p):
 def check(p, hash):
     return bcrypt.checkpw(p.encode(), hash.encode())
 
+@app.before_request
 def is_right_api_key():
-    return request.headers.get("X-API-KEY") == API_KEY
-@app.route("/api/sign-up", methods=["POST"])
-def sign_up():
-    if is_right_api_key():
-        data = request.get_json()
-        username = data.get("username")
-        password = data.get("password")
-        if (not isinstance(username, str)) or (not isinstance(password, str)):
-            return jsonify({"status": "error", "reason": "input is not string"})
-        if username in passwords:
-            return jsonify({"status": "error", "reason": "username already exists"})
-        passwords[username] = hash(password)
-        with open("users.json", "w") as f:
-            json.dump(passwords, f)
-        return jsonify({"status": "success"})
-    else:
+    if not request.headers.get("X-API-KEY") == API_KEY:
         return jsonify({"status": "error", "reason": "unauthorized request"}), 401
 
+@app.route("/api/sign-up", methods=["POST"])
+def sign_up():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if (not isinstance(username, str)) or (not isinstance(password, str)):
+        return jsonify({"status": "error", "reason": "input is not string"})
+    if username in passwords:
+        return jsonify({"status": "error", "reason": "username already exists"})
+    passwords[username] = hash(password)
+    with open("users.json", "w") as f:
+        json.dump(passwords, f)
+    return jsonify({"status": "success"})
+        
 @app.route("/api/log-in", methods=["POST"])
 def log_in():
-    if is_right_api_key():
-        data = request.get_json()
-        username = data.get("username")
-        password = data.get("password")
-        if not username in passwords:
-            return jsonify({"status": "error", "reason": "no user with this username exists"})
-        if check(password, passwords[username]):
-            return jsonify({"status": "success"})
-        else:
-            return jsonify({"status": "error", "reason": "password incorrect"})
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if not username in passwords:
+        return jsonify({"status": "error", "reason": "no user with this username exists"})
+    if check(password, passwords[username]):
+        return jsonify({"status": "success"})
     else:
-        return jsonify({"status": "error", "reason": "unauthorized request"}), 401
+        return jsonify({"status": "error", "reason": "password incorrect"})
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
